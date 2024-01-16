7cb84dcb108945a7967d8f930cc23cbfc374cb9f
Alex <alexstocking@hotmail.co.uk>
2024-01-16 16:05:50 +0000

    working backend for CRUDable pokedex

--- index.js
+++ index.js
@@ -18,15 +18,16 @@ app.listen(port, () => {
 mongoose.connect(process.env.DATABASE_URL)
 let pokeUrl = process.env.POKEDATABASE_URL
 let imageUrl = process.env.IMAGE_URL
+let altImageUrl = process.env.ALT_IMAGE_URL
 
 const pokemonSchema = new mongoose.Schema({
     id: Number,
     name: String,
     types: Array,
     abilities: Array,
-    hidAbility: String,
+    hiddenAbility: String,
     evolvesFrom: String,
-    baseStatNumbers: {
+    baseStats: {
         hp: Number,
         attack: Number,
         defense: Number,
@@ -34,17 +35,22 @@ const pokemonSchema = new mongoose.Schema({
         specialDefense: Number,
         speed: Number,
     },
-    img: String
+    image: String
 })
 
 const userSchema = new mongoose.Schema({
     userEmail: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     lastLogin: {
         type: Date,
         required: true
+    },
+    customPokemon: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pokemon'
     }
 })
 
@@ -80,7 +86,12 @@ app.get('/pokemon', (req, res) => {
                         return pokemonDetailResponse.json();
                     })
                     .then((pokemonDetailData) => {
-                        const uniqueImg = `${imageUrl}/${pokemonDetailData.id}.png`;
+                        let uniqueImg = ''
+                        if (pokemonDetailData.id < 1018 && pokemonDetailData.id !== 1013) {
+                            uniqueImg = `${imageUrl}/${pokemonDetailData.id}.png`
+                        } else {
+                            uniqueImg = `${altImageUrl}/${pokemonDetailData.id}.png`
+                        }
 
                         return {
                             id: pokemonDetailData.id,
@@ -107,95 +118,100 @@ app.get('/pokemon', (req, res) => {
         });
 });
 
-app.get('/pokemon/:id', (req, res) => {
+app.get('/pokemon/:id', async (req, res) => {
     const id = req.params.id;
     const pokeApiUrl = `${pokeUrl}/pokemon/${id}`;
     const pokeSpeciesApiUrl = `${pokeUrl}/pokemon-species/${id}`;
 
-    // Check if custom Pokémon exists in MongoDB
-    Pokemon.findOne({ id: id }, { _id: 0, __v: 0 })
-        .then((customPokemon) => {
-            if (customPokemon) {
-                if (!res.headersSent) {
-                    // If response hasn't been sent, send the custom Pokémon
-                    res.json(customPokemon);
+    try {
+        // Check if custom Pokémon exists in MongoDB
+        const customPokemon = await Pokemon.findOne({ id: id }, { _id: 0, __v: 0 });
+
+        if (customPokemon) {
+            // If custom Pokémon exists, return it
+            res.json(customPokemon);
+        } else {
+            // Fetch Pokémon details from the PokeAPI
+            const [pokemonResponse, speciesResponse] = await Promise.all([
+                fetch(pokeApiUrl),
+                fetch(pokeSpeciesApiUrl),
+            ]);
+
+            if (!pokemonResponse.ok || !speciesResponse.ok) {
+                throw new Error('Pokémon not found');
+            }
+
+            const [pokemonData, speciesData] = await Promise.all([
+                pokemonResponse.json(),
+                speciesResponse.json(),
+            ]);
+
+            const regularAbilities = [];
+            const hiddenAbilities = [];
+
+            pokemonData.abilities.forEach((ability) => {
+                if (ability.is_hidden) {
+                    hiddenAbilities.push(ability.ability.name);
+                } else {
+                    regularAbilities.push(ability.ability.name);
                 }
-            } else {
-                // Fetch Pokémon from the PokeAPI
-                return Promise.all([
-                    fetch(pokeApiUrl),
-                    fetch(pokeSpeciesApiUrl),
-                ]).then(([pokemonResponse, speciesResponse]) => {
-                    // Check if the response is not OK (e.g., 404 Not Found)
-                    if (!pokemonResponse.ok || !speciesResponse.ok) {
-                        throw new Error('Pokémon not found');
-                    }
+            });
 
-                    // Parse the responses as JSON
-                    return Promise.all([
-                        pokemonResponse.json(),
-                        speciesResponse.json(),
-                    ]);
-                });
-            }
-        })
-        .then(([pokemonData, speciesData]) => {
-            // Check if response has already been sent
-            if (!res.headersSent) {
-                const regularAbilities = [];
-                const hiddenAbilities = [];
+            // Process and save the updated Pokémon details to MongoDB
+            const updatedPokemon = {
+                id: pokemonData.id,
+                name: pokemonData.name,
+                types: pokemonData.types.map((type) => type.type.name),
+                abilities: regularAbilities,
+                hiddenAbility: hiddenAbilities.length > 0 ? hiddenAbilities[0] : null,
+                evolvesFrom: speciesData.evolves_from_species ? speciesData.evolves_from_species.name : null,
+                baseStats: {
+                    hp: pokemonData.stats[0].base_stat,
+                    attack: pokemonData.stats[1].base_stat,
+                    defense: pokemonData.stats[2].base_stat,
+                    specialAttack: pokemonData.stats[3].base_stat,
+                    specialDefense: pokemonData.stats[4].base_stat,
+                    speed: pokemonData.stats[5].base_stat,
+                },
+                image: getUniqueImageUrl(pokemonData.id),
+            };
 
-                pokemonData.abilities.forEach((ability) => {
-                    if (ability.is_hidden) {
-                        hiddenAbilities.push(ability.ability.name);
-                    } else {
-                        regularAbilities.push(ability.ability.name);
-                    }
-                });
+            // Save the updated Pokémon details to MongoDB
+            await Pokemon.findOneAndUpdate({ id: id }, updatedPokemon, { upsert: true });
 
-                const uniqueImg = `${imageUrl}/${pokemonData.id}.png`;
-
-                res.json({
-                    id: pokemonData.id,
-                    name: pokemonData.name,
-                    types: pokemonData.types.map((type) => type.type.name),
-                    abilities: regularAbilities,
-                    hiddenAbility: hiddenAbilities.length > 0 ? hiddenAbilities[0] : null,
-                    evolvesFrom: speciesData.evolves_from_species ? speciesData.evolves_from_species.name : null,
-                    baseStats: {
-                        hp: pokemonData.stats[0].base_stat,
-                        attack: pokemonData.stats[1].base_stat,
-                        defense: pokemonData.stats[2].base_stat,
-                        specialAttack: pokemonData.stats[3].base_stat,
-                        specialDefense: pokemonData.stats[4].base_stat,
-                        speed: pokemonData.stats[5].base_stat,
-                    },
-                    image: uniqueImg,
-                });
-            }
-        })
-        .catch((error) => {
-            // Check if response has already been sent
-            if (!res.headersSent) {
-                console.error(error);
-                res.status(404).json({
-                    error: 'Pokémon not found',
-                    details: error.message,
-                });
-            }
+            // Return the updated Pokémon details, including the image URL
+            res.json(updatedPokemon);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(404).json({
+            error: 'Pokémon not found',
+            details: error.message,
         });
+    }
 });
 
+
+function getUniqueImageUrl(id) {
+    if (id < 1018 && id !== 1013) {
+        return `${imageUrl}/${id}.png`;
+    } else if (id === 1013 || id > 1017) {
+        return `${altImageUrl}/${id}.png`;
+    } else {
+        // Handle other cases or return a default image URL
+        return `${imageUrl}/${id}.png`;
+    }
+}
 app.post('/pokemon/add', (req, res) => {
     const {
         id,
         name,
         types,
         abilities,
-        hidAbility,
+        hiddenAbility,
         evolvesFrom,
-        baseStatNumbers,
-        img
+        baseStats,
+        image
     } = req.body;
 
     const newPokemon = new Pokemon({
@@ -203,13 +219,17 @@ app.post('/pokemon/add', (req, res) => {
         name,
         types,
         abilities,
-        hidAbility,
+        hiddenAbility,
         evolvesFrom,
-        baseStatNumbers,
-        img
+        baseStats,
+        image
     });
 
+    const userId = req.body.userId
+    newPokemon.user = userId
+
     newPokemon.save()
+        .then(() => User.findByIdAndUpdate(userId, {customPokemon: newPokemon._id}))
         .then(() => {
             res.status(201).json({
                 message: 'Pokemon added successfully',
@@ -224,6 +244,35 @@ app.post('/pokemon/add', (req, res) => {
             });
         });
 });
+
+app.delete('/pokemon/:id', (req, res) => {
+    Pokemon.deleteOne({id: req.params.id})
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        res.sendStatus(500)
+    })
+})
+
+app.put('/pokemon/:id', (req, res) => {
+    Pokemon.updateOne({id: req.params.id}, {
+        id: req.body.id,
+        name: req.body.name,
+        types: req.body.types,
+        abilities: req.body.abilities,
+        hiddenAbility: req.body.hiddenAbility,
+        evolvesFrom: req.body.evolvesFrom,
+        baseStats: req.body.baseStats,
+        image: req.body.image
+    })
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(err => {
+        res.sendStatus(500)
+    })
+})
 
 app.post('/user/login', async (req, res) => {
     const now = new Date()
@@ -244,14 +293,4 @@ app.post('/user/login', async (req, res) => {
         await User.findOneAndUpdate({"userEmail": req.body.userEmail}, {lastLogin: now})
         res.sendStatus(200)
     }
-})
-
-app.delete('/pokemon/:id', (req, res) => {
-    Pokemon.deleteOne({id: req.params.id})
-    .then(() => {
-        res.sendStatus(200)
-    })
-    .catch(err => {
-        res.sendStatus(500)
-    })
 })
